f1b571c13c05c0d3edc2375a48e08febaf863b7b
LangYa466 <langya466@gmail.com>
2025-03-20 18:36:22 +0800

    values

--- src/main/java/cn/firefox/manager/ModuleManager.java
+++ src/main/java/cn/firefox/manager/ModuleManager.java
@@ -6,9 +6,12 @@ import cn.firefox.module.Module;
 import cn.firefox.module.impl.combat.*;
 import cn.firefox.module.impl.move.*;
 import cn.firefox.module.impl.render.*;
+import cn.firefox.module.value.Value;
+import cn.langya.Logger;
 import com.cubk.event.annotations.EventTarget;
 import lombok.Getter;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +25,17 @@ public class ModuleManager {
 
     private void addModule(Module module) {
         moduleMap.put(module.getName(), module);
+
+        for (final Field field : module.getClass().getDeclaredFields()) {
+            try {
+                field.setAccessible(true);
+                final Object obj = field.get(module);
+                if (obj instanceof Value<?>) module.getValues().add((Value<?>) obj);
+            } catch (IllegalAccessException e) {
+                Logger.error(e.getMessage());
+            }
+        }
+
         // test code
         module.setEnabled(true);
     }
--- src/main/java/cn/firefox/module/Module.java
+++ src/main/java/cn/firefox/module/Module.java
@@ -2,8 +2,12 @@ package cn.firefox.module;
 
 import cn.firefox.Client;
 import cn.firefox.Wrapper;
+import cn.firefox.module.value.Value;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author LangYa466
@@ -16,6 +20,8 @@ public class Module implements Wrapper {
     private final Category category;
     private String suffix;
     private int keyCode;
+
+    private List<Value<?>> values = new ArrayList<>();
 
     public Module(String name, Category category) {
         this.name = name;
